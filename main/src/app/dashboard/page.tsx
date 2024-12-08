315c807818dff6beccb74ce94f96b1e3ceafdea8
ak220193 <mail2meak22frcrio@gmail.com>
2024-12-08 10:00:31 +0530

    Dashboard Page

---
+++ main/src/app/dashboard/page.tsx
@@ -0,0 +1,49 @@
+
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { UserButton } from "@clerk/nextjs";
+import { FaArrowLeft } from "react-icons/fa6";
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
+
+type Props = {};
+
+const DashboardPage = async (props: Props) => {
+
+
+  return (
+    <>
+      <div className="min-h-screen">
+        <div className="max-w-7xl mx-auto p-10">
+          <div className="h-14"></div>
+          <div className="flex justify-between items-center md:flex-row flex-col">
+            <div className="flex items-center">
+              <Link href="/">
+                <Button className="bg-green-600" size="sm">
+                  <FaArrowLeft className="mr-1 w-4 h-4" />
+                  Back
+                </Button>
+              </Link>
+              <div className="w-4"></div>
+              <h1 className="text-3xl font-bold text-gray-900">My Notes</h1>
+              <div className="w-4"></div>
+              <UserButton />
+            </div>
+          </div>
+
+          <div className="h-8"></div>
+          <Separator />
+          <div className="h-8"></div>
+          {/* list all the notes */}
+          {/* if no notes, display this */}
+            <div className="text-center">
+              <h2 className="text-xl text-gray-500">You have no notes yet.</h2>
+            </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DashboardPage;
